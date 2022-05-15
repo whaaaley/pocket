5eb11427d469ae600dd9465243bbd1fa2d17d767
Whaley <dustindowell22@gmail.com>
2022-05-14 21:21:02 -0500

    Update graphql.js

--- toolchain/plugins/graphql.js
+++ toolchain/plugins/graphql.js
@@ -1,17 +1,2 @@
 
-import babel from '@babel/core'
-import config from '../config.js'
-
-function handler (args) {
-  return {
-    loader: 'jsx',
-    contents: babel.transformFileSync(args.path, config.babel).code
-  }
-}
-
-export default {
-  name: 'plugin-babel',
-  setup (build) {
-    build.onLoad({ filter: /\.(js|jsx)$/ }, handler)
-  }
-}
+// TODO
