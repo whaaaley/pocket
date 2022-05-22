23964034e409f790c78e7a0b93f13ee6b9dfb117
Whaley <dustindowell22@gmail.com>
2022-05-21 22:43:13 -0500

    Format src/index.js

--- src/index.js
+++ src/index.js
@@ -14,8 +14,6 @@ function Page (props) {
     <meta name='viewport' content={props.viewport}/>
     <link rel='icon' href='/cache/logo.svg'/>
     <style>{'<!-- xxx -->' + style}</style>
-    {/* <link rel='preload' href='/fonts/Inter-3.18/Inter-roman.var.woff2' as='font' type='font/woff2' crossorigin></link> */}
-    {/* <link rel='preload' href='/fonts/SourceCodePro-1.018/SourceCodeVariable-Roman.ttf.woff2' as='font' type='font/woff2' crossorigin></link> */}
     <body>
       <noscript>Please enable JavaScript and try again.</noscript>
       <div id='pocket'></div>
@@ -25,10 +23,16 @@ function Page (props) {
   </html>
 }
 
+const desc = 'Pocket is a library for building small applications and' +
+  ' universal components that can be rendered by any framework that uses' +
+  ' hyperscript functions or JSX. Like many frameworks, it provides a' +
+  ' component-based model to create interfaces combined with state' +
+  ' management, page routing, and style encapsulation.'
+
 const html = Page({
-  title: 'Pocket Micro Framework | A tiny library for building small applications.',
+  title: 'Pocket Micro Framework',
   author: 'Dustin Dowell',
-  description: 'Pocket is a library for building small applications and universal components that can be rendered by any framework that uses hyperscript functions or JSX. Like many frameworks, it provides a component-based model to create interfaces combined with state management, page routing, and style encapsulation.',
+  description: desc,
   viewport: 'width=device-width,maximum-scale=5'
 })
 
