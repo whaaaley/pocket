90823cf988f01178161332da9bf942332c5f37a9
Whaley <dustindowell22@gmail.com>
2022-06-05 05:37:08 -0500

    Rename plugin resolution to resolve

---
+++ toolchain/plugins/resolve.js
@@ -0,0 +1,33 @@
+
+import fs from 'fs'
+import path from 'path'
+
+export default function ({ loadpaths }) {
+  function handler (args) {
+    // TODO: remove dups
+
+    for (let index = 0; index < loadpaths.length; index++) {
+      let pathname = path.join(loadpaths[index], args.path.slice(2))
+
+      try {
+        // NOTE: fs.lstatSync will throw an error if the path doesn't exist
+        if (fs.lstatSync(pathname).isDirectory()) {
+          pathname = path.join(pathname, '/index.js')
+        }
+
+        return {
+          path: pathname
+        }
+      } catch {
+        // Do nothing...
+      }
+    }
+  }
+
+  return {
+    name: 'plugin-resolve',
+    setup (build) {
+      build.onResolve({ filter: /^~\// }, handler)
+    }
+  }
+}
