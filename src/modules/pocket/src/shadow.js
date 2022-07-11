776d04c26b6935c7c671c4663efa701b1e2f536b
whaaaley <dustindowell22@gmail.com>
2022-07-11 05:00:20 -0500

    Update shadow.js

--- src/modules/pocket/src/shadow.js
+++ src/modules/pocket/src/shadow.js
@@ -51,6 +51,8 @@ export function IFrameRoot ({ host, patch }, view) {
 }
 
 export function Component ({ host, init, props, patch }, view) {
+  console.warn('Warning: Component is deprecated. Use defineComponent instead.')
+
   let node
 
   Object.defineProperty(host, 'node', {
@@ -70,8 +72,8 @@ export function Component ({ host, init, props, patch }, view) {
         // cache original setup function
         const setup = init.setup
 
-        init.setup = function (state, dispatch) {
-          const render = setup(state, dispatch)
+        init.setup = function (state) {
+          const render = setup(state)
 
           // eslint-disable-next-line no-return-assign
           return data.render = function () {
@@ -82,7 +84,6 @@ export function Component ({ host, init, props, patch }, view) {
         root = node.attachShadow(shadowInit)
         root = root.appendChild(data.root = document.createElement('div'))
 
-        // should i unwrap this callback?
         core(init, function (view) {
           return patch(root, view)
         })
@@ -93,4 +94,48 @@ export function Component ({ host, init, props, patch }, view) {
   })
 
   return host
+}
+
+export function defineComponent ({ host, patch, props, children }, setup2) {
+  let node
+
+  function get () {
+    return node
+  }
+
+  function set (value) {
+    const cache = nodeMap.get(node = value) ?? {}
+
+    cache.props = props
+    cache.children = children
+
+    if (cache.root) {
+      patch(cache.root, cache.render(props, children))
+      return // early exit
+    }
+
+    const render = setup2(function useState (state2) {
+      // eslint-disable-next-line no-return-assign
+      return cache.state = state2
+    }, node)
+
+    function setup () {
+      // eslint-disable-next-line no-return-assign
+      return cache.render = function () {
+        return render(cache.props, cache.children)
+      }
+    }
+
+    const root = node
+      .attachShadow(shadowInit)
+      .appendChild(cache.root = document.createElement('div'))
+
+    core({ state: cache.state, setup }, function (view) {
+      return patch(root, view)
+    })
+
+    nodeMap.set(node, cache)
+  }
+
+  return Object.defineProperty(host, 'node', { get, set })
 }
