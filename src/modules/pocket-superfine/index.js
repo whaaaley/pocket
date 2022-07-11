6a55860d277d7c5cbae9e9e84986d8c003427d13
whaaaley <dustindowell22@gmail.com>
2022-07-11 05:02:09 -0500

    Export defineComponent from pocket-superfine

--- src/modules/pocket-superfine/index.js
+++ src/modules/pocket-superfine/index.js
@@ -2,3 +2,4 @@
 export { core, app } from './src/setup.js'
 export { ShadowRoot, IFrameRoot, Component } from './src/shadow.js'
 export { default as Async } from './src/async.js'
+export { defineComponent } from './src/shadow.js'
