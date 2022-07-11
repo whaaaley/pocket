2f7b2445c2db4465334f16eade5a4aaea9713da7
whaaaley <dustindowell22@gmail.com>
2022-07-11 05:00:44 -0500

    Export defineComponent from pocket

--- src/modules/pocket/index.js
+++ src/modules/pocket/index.js
@@ -2,3 +2,4 @@
 export { default as pocket, core } from './src/pocket.js'
 export { link, router } from './src/router.js'
 export { Component, IFrameRoot, ShadowRoot } from './src/shadow.js'
+export { defineComponent } from './src/shadow.js'
