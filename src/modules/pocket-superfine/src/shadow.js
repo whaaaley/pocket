a41916bd91a89ceaae05bda2a06f7108d113d1d3
whaaaley <dustindowell22@gmail.com>
2022-07-11 05:01:06 -0500

    Add defineComponent to pocket-superfine

--- src/modules/pocket-superfine/src/shadow.js
+++ src/modules/pocket-superfine/src/shadow.js
@@ -1,14 +1,30 @@
 
-import { h, patch } from 'superfine'
-import { ShadowRoot, IFrameRoot, Component } from '~/modules/pocket/'
-import { copy } from './lib/'
+import { h, text, patch } from 'superfine'
+import { ShadowRoot, IFrameRoot, Component, defineComponent } from '~/modules/pocket/'
 
 function ShadowRoot2 ({ id, styles, slots }, children) {
   const key = id + '-host'
   const host = h('div', { key, id: key }, [])
 
-  copy('style', styles, children)
-  copy('slot', slots, host.children)
+  if (styles) {
+    for (const key in styles) {
+      const styleKey = id + '-style-' + children.length
+
+      children.push(
+        h('style', { key: styleKey }, text(styles[key]))
+      )
+    }
+  }
+
+  if (slots) {
+    for (const key in slots) {
+      const slotKey = id + '-slot-' + host.children.length
+
+      host.children.push(
+        h('slot', { slot: key, key: slotKey }, slots[key])
+      )
+    }
+  }
 
   return ShadowRoot({ host, patch }, h('div', { id }, children))
 }
@@ -17,29 +33,56 @@ function IFrameRoot2 ({ id, styles }, children) {
   const key = id + '-host'
   const host = h('iframe', { key, id: key })
 
-  copy('slot', styles, children)
+  if (styles) {
+    for (const key in styles) {
+      const styleKey = id + '-style-' + children.length
+
+      children.push(
+        h('style', { key: styleKey }, text(styles[key]))
+      )
+    }
+  }
 
   return IFrameRoot({ host, patch }, h('div', { id }, children))
 }
 
 function Component2 ({ id, init, props, styles, slots }, children) {
-  const key = id + '-host'
-  const host = h('div', { key, id: key }, [])
+  console.warn('Warning: Component is deprecated. Use defineComponent instead.')
 
-  copy('slot', slots, host.children)
+  const hostKey = id + '-host'
+  const host = h('div', { key: hostKey, id: hostKey }, [])
+
+  if (slots) {
+    for (const key in slots) {
+      const slotKey = id + '-slot-' + host.children.length
+
+      host.children.push(
+        h('slot', { key: slotKey, slot: key }, slots[key])
+      )
+    }
+  }
 
   // cache original setup function
   const setup = init.setup
 
-  init.setup = function (state, dispatch) {
-    const render = setup(state, dispatch)
+  init.setup = function (state) {
+    const render = setup(state)
 
     return function (props2, children2) {
       let vdom = render(props2, children2)
 
       // children cannot be guaranteed to be an array
       vdom = Array.isArray(vdom) ? vdom : [vdom]
-      copy('style', styles, vdom)
+
+      if (styles) {
+        for (const key in styles) {
+          const styleKey = id + '-style-' + vdom.length
+
+          vdom.push(
+            h('style', { id: styleKey, key: styleKey }, text(styles[key]))
+          )
+        }
+      }
 
       return h('div', { id }, vdom)
     }
@@ -48,8 +91,49 @@ function Component2 ({ id, init, props, styles, slots }, children) {
   return Component({ host, init, props, patch }, children)
 }
 
+function defineComponent2 ({ id, styles, slots, props, children }, setup2) {
+  const key = id + '-host'
+  const host = h('div', { key, id: key }, [])
+
+  if (slots) {
+    for (const key in slots) {
+      const slotKey = id + '-slot-' + host.children.length
+
+      host.children.push(
+        h('slot', { key: slotKey, slot: key }, slots[key])
+      )
+    }
+  }
+
+  function setup (useState) {
+    const render = setup2(useState)
+
+    return function (props2, children2) {
+      let ch = render(props2, children2)
+
+      // children cannot be guaranteed to be an array
+      ch = Array.isArray(ch) ? ch : [ch]
+
+      if (styles) {
+        for (const key in styles) {
+          const styleKey = id + '-style-' + host.children.length
+
+          ch.push(
+            h('style', { key: styleKey }, text(styles[key]))
+          )
+        }
+      }
+
+      return h('div', { id }, ch)
+    }
+  }
+
+  return defineComponent({ host, patch, props, children }, setup)
+}
+
 export {
   ShadowRoot2 as ShadowRoot,
   IFrameRoot2 as IFrameRoot,
-  Component2 as Component
+  Component2 as Component,
+  defineComponent2 as defineComponent
 }
