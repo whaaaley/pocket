d1e5bbe2faa6ec16da0b345de0f38b3a188c3190
whaaaley <dustindowell22@gmail.com>
2022-07-24 00:43:41 -0500

    Create media-types.js

---
+++ deploy/media-types.js
@@ -0,0 +1,48 @@
+
+// TODO: Find good default values for these
+export default {
+  '.css': {
+    'content-type': 'text/css',
+    'cache-control': 'max-age=31536000'
+  },
+  '.html': {
+    'content-type': 'text/html',
+    'cache-control': 'max-age=31536000'
+  },
+  '.jpg': {
+    'content-type': 'image/jpeg',
+    'cache-control': 'max-age=31536000'
+  },
+  '.js': {
+    'content-type': 'application/javascript',
+    'cache-control': 'no-cache'
+  },
+  '.json': {
+    'content-type': 'application/json',
+    'cache-control': 'no-cache'
+  },
+  '.md': {
+    'content-type': 'text/markdown',
+    'cache-control': 'no-cache'
+  },
+  '.ogg': {
+    'content-type': 'audio/ogg',
+    'cache-control': 'max-age=31536000'
+  },
+  '.png': {
+    'content-type': 'image/png',
+    'cache-control': 'max-age=31536000'
+  },
+  '.svg': {
+    'content-type': 'image/svg+xml',
+    'cache-control': 'max-age=31536000'
+  },
+  '.woff': {
+    'content-type': 'font/woff',
+    'cache-control': 'max-age=31536000'
+  },
+  '.woff2': {
+    'content-type': 'font/woff2',
+    'cache-control': 'max-age=31536000'
+  }
+}
