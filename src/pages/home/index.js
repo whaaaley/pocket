ed7f3b0c43206a5d4596a8e675cff0b9be56926f
Dustin Dowell <7419947+whaaaley@users.noreply.github.com>
2023-01-22 04:37:39 -0600

    Update homepage example

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,6 +1,7 @@
 
 import { ShadowRoot, AsyncComponent } from '~/modules/pocket-superfine'
 import homeStyles from './_home.scss'
+
 import Layout from '~/components/layout'
 import Hero from './hero.js'
 
@@ -31,33 +32,3 @@ export default {
     // Nothing yet...
   }
 }
-
-// import { Async, ShadowRoot } from '~/modules/pocket-superfine'
-// import style from './_home.scss'
-//
-// import Layout from '~/components/layout'
-// import Hero from './hero.js'
-//
-// export default {
-//   setup (state, dispatch) {
-//     return function () {
-//       return <div id='page-home'>
-//         <ShadowRoot id='home' styles={[style]}>
-//           <Layout>
-//             <Hero/>
-//             <div class='page'>
-//               <Async module={import('./content.mdx')}>
-//                 <div style='min-height: 1309px'>
-//                   {/* CLS - Height from Chrome DevTools */}
-//                 </div>
-//               </Async>
-//             </div>
-//           </Layout>
-//         </ShadowRoot>
-//       </div>
-//     }
-//   },
-//   destroy () {
-//     // ...
-//   }
-// }
